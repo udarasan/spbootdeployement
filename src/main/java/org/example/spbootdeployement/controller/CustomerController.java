7979bc260121b411e9e9cacd9075f1bac88dacb0
Udara San <udarassanjeewa@gmail.com>
2025-08-04 10:25:27 +0530

    updates

--- src/main/java/org/example/spbootdeployement/controller/CustomerController.java
+++ src/main/java/org/example/spbootdeployement/controller/CustomerController.java
@@ -1,4 +1,14 @@
 package org.example.spbootdeployement.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
+    @GetMapping
+    public String getCustomer() {
+        return "Customer 1";
+    }
 }
