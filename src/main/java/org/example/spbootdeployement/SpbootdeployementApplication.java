065fbf87d8c4257534498f60c2605cb24a547a5c
Udara San <udarassanjeewa@gmail.com>
2025-08-04 10:01:38 +0530

    Initial commit

---
+++ src/main/java/org/example/spbootdeployement/SpbootdeployementApplication.java
@@ -0,0 +1,13 @@
+package org.example.spbootdeployement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootdeployementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootdeployementApplication.class, args);
+	}
+
+}
